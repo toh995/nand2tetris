c44920789a38e243a00c2ee90d690a02d8881867
toh995 <52012721+toh995@users.noreply.github.com>
2024-05-12 13:41:14 -0700

    project 04 solutions

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,83 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// max_addr = KBD - 1
+@KBD
+D=A
+@max_addr
+M=D-1
+
+// addr = SCREEN
+@SCREEN
+D=A
+@addr
+M=D
+
+// curr_color = 1
+@curr_color
+M=0
+
+@LOOP
+0; JMP
+
+(LOOP)
+  @KBD
+  D=M
+  @WHITEN
+  D; JEQ
+  @BLACKEN
+  0; JMP
+
+(WHITEN)
+  @curr_color
+  D=M
+  M=0
+  @UPDATE_EXISTING
+  D; JEQ
+  @UPDATE_NEW
+  0; JMP
+
+(BLACKEN)
+  @curr_color
+  D=M
+  M=-1
+  @UPDATE_NEW
+  D; JEQ
+  @UPDATE_EXISTING
+  0; JMP
+
+(UPDATE_NEW)
+  // Update the initial screen register
+  @curr_color
+  D=M
+  @SCREEN
+  M=D
+  // Save the next addr
+  D=A
+  @addr
+  M=D+1
+  // Go back to the main loop
+  @LOOP
+  0; JMP
+  
+(UPDATE_EXISTING)
+  // If addr > max_addr,
+  // then go back to the main loop
+  @addr
+  D=M
+  @max_addr
+  D=M-D
+  @LOOP
+  D; JLT
+  // Update addr's screen register
+  @curr_color
+  D=M
+  @addr
+  A=M
+  M=D
+  // addr++
+  @addr
+  M=M+1
+  // Return to the main loop
+  @LOOP
+  0; JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,4 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// R2 = 0
+// for (i = R1; i > 0; i--):
+//    R2 = R2 + R0
+
+// R2 = 0
+@R2
+M=0
+
+// i = R1
+@R1
+D=M
+@i
+M=D
+
+// Start the loop
+@LOOP
+0; JMP
+
+(LOOP)
+  // if i <= 0, then goto END
+  @i
+  D=M
+  @END
+  D; JLE
+
+  // R2 = R2 + R0
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  // i--
+  @i
+  M=M-1
+
+  // Restart the loop
+  @LOOP
+  0; JMP
+
+// Infinite loop
+(END)
+  @END
+  0; JMP
